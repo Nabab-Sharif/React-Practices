cb27c5279c9d5321d3ecee45439b9e2cde369fe4
md nabab sharif <mdnababsharifdiu@gmail.com>
2022-11-30 22:38:47 +0600

    State Create

--- src/component/home/HomeBanner.js
+++ src/component/home/HomeBanner.js
@@ -7,16 +7,36 @@ export default class HomeBanner extends Component {
 
   constructor(props) {
     super();
+    this.state = {
+      color: "Red"
+    }
   }
 
+  BlueColor = () => {
+    this.setState({
+      color: "Blue"
+    })
+  }
 
+  OrangeColor=()=>{
+    this.setState({
+      color:"Orange"
+    })
+  }  
 
+  GreenColor=()=>{
+    this.setState({
+      color:"Green"
+    })
+  }
 
   render() {
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        <p>{this.props.subtitle}</p>
+        <h1>{this.state.color}</h1>
+        <button onClick={this.BlueColor}>Blue</button>
+        <button onClick={this.OrangeColor}>Orange</button>
+        <button onClick={this.GreenColor}>Green</button>
       </div>
     )
   }
